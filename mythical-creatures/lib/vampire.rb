d61f10bebf7d38fade8a85aeb32094a1519c8c35
Ryan Camp <cmpprg@icloud.com>
2020-01-23 13:57:26 -0700

    Complete vampire class exercise

---
+++ mythical-creatures/lib/vampire.rb
@@ -0,0 +1,20 @@
+class Vampire
+  attr_reader :name, :pet
+  attr_accessor :thirsty
+
+  def initialize(name, pet = "bat", thirsty = true)
+    @name = name
+    @pet = pet
+    @thirsty = thirsty
+
+  end
+
+  def thirsty?
+    thirsty
+  end
+
+  def drink
+    self.thirsty = false
+  end
+
+end
