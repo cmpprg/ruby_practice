3036e0c3e6ce5a6d056cfe940b094e3ebec0c375
Ryan Camp <cmpprg@icloud.com>
2020-01-24 12:00:34 -0700

    Complete leather_chair test exercise

--- command-query/exercises/leather_chair_test.rb
+++ command-query/exercises/leather_chair_test.rb
@@ -7,11 +7,11 @@ class LeatherChairTest < Minitest::Test
 
   def test_a_new_chair_is_not_faded
     chair = LeatherChair.new
-    refute chair.faded?, "New chairs are NOT faded."
+    refute chair.faded?
   end
 
   def test_exposing_a_chair_to_sunlight_makes_it_fade
-    skip
+
     chair = LeatherChair.new
     chair.expose_to_sunlight
     assert chair.faded?, "Exposed chairs are faded."
